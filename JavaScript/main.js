f3556fde7fc122b0845e08701d59c257235dc53b
alexquantzflemingc <114168893+alexquantzflemingc@users.noreply.github.com>
2022-12-08 21:52:06 -0500

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,38 @@
+// By Amanda 
+// External javascript file to be referenced on index and programming page
+// Functions defined in javascript file, to be called in main html file
+// All adapted from w3schools
+
+// Change text function
+// Changes html content upon event 
+function changeTxt() {
+  document.getElementById("javstxt").innerHTML = "Just kidding";
+}
+
+// Change text size function
+function bigTxt() {
+  document.getElementById('big').style.fontSize='100px';
+}
+// Alert functions
+// Causes alert upon event
+function alrt() {
+  alert("JavaScript alert");
+}
+function inpt() {
+  alert("Invalid address");
+}
+
+
+
+
+// By Josh
+// Alert function for Geovisualization Page
+function meow() {
+  alert("Have you seen my baseball?");
+}
+
+// Alert function for Spatial Analysis Page
+function meow() {
+  alert("Have you seen my Dad?");
+}
+
